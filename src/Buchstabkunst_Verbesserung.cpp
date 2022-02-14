dec29c6f3d2e2f2c88e87780e00de7495b19306d
Claegtun <clayton.john.carlon@gmail.com>
2022-02-14 11:00:51 +1100

    Added input file

--- src/Buchstabkunst_Verbesserung.cpp
+++ src/Buchstabkunst_Verbesserung.cpp
@@ -2,14 +2,26 @@
 // Name        : Buchstabkunst_Verbesserung.cpp
 // Author      : Claegtun
 // Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Copyright   :
+// Description : A simple program to make ASCII art from a .PNG file
 //============================================================================
 
-#include <iostream>
+#include <iostream> //cout
+#include <fstream> //file IO
 using namespace std;
 
 int main() {
-	cout << "!!!Heallo World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Begin." << endl;
+
+	//Open the input file.
+	ifstream smInput("input.png");
+	if (!smInput.is_open())
+	{
+		cout << "File could not be opened." << endl;
+		return 1;
+	}
+
+
+	cout << "End." << endl;
 	return 0;
 }
